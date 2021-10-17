abf6ddbc08faaf05db7ca8f249ba9c1669ca14d7
Shiyang Wang <shiyangwang@umass.edu>
2021-10-17 16:22:15 -0400

    update singleton

--- Server/api/care_api/device_manager/zeromq_lib.py
+++ Server/api/care_api/device_manager/zeromq_lib.py
@@ -1,24 +1,27 @@
 import os, json
-import zmq 
+import zmq
 from threading import Thread, RLock
 
 ZEROMQ_SERVER = "0.0.0.0"
-ZEROMQ_PORT   = "5456"
+ZEROMQ_PORT = "5456"
 
-class ZeroMQ_Manager:
-	def __init__(self):
-		context = zmq.Context()
-		self.socket = context.socket(zmq.PUB)
-		self.socket.bind(f"tcp://{ZEROMQ_SERVER}:{ZEROMQ_PORT}")
 
-		self.data_topic = "userdata"
-		print(f"tcp://{ZEROMQ_SERVER}:{ZEROMQ_PORT}")
+class ZeroMQ_Manager():
 
-	def send(self, data):
-		try:
-			self.socket.send_string("%s %s" % (self.data_topic, json.dumps(data)))
-		except Exception as e:
-			pass 
+    def __init__(self):
+        if self.socket is None:
+            print('Creating the ZeroMQ_Manager singleton')
+            print(f"tcp://{ZEROMQ_SERVER}:{ZEROMQ_PORT}")
+            context = zmq.Context()
+            self.socket = context.socket(zmq.PUB)
+            self.socket.bind(f"tcp://{ZEROMQ_SERVER}:{ZEROMQ_PORT}")
+            self.data_topic = "userdata"
+
+    def send(self, data):
+        try:
+            self.socket.send_string("%s %s" % (self.data_topic, json.dumps(data)))
+        except Exception as e:
+            pass
 
 
 zeroMQManager = ZeroMQ_Manager()
