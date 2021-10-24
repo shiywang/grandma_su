5557e8a09c3e11b07c1c3d769fa80c1a5f296e86
Shiyang Wang <shiywang.cs@gmail.com>
2021-10-23 21:49:24 -0400

    change entrypoint to use ssl server

--- Server/api/entrypoint.sh
+++ Server/api/entrypoint.sh
@@ -4,4 +4,4 @@ python care_api/manage.py makemigrations --noinput
 python care_api/manage.py migrate
 DJANGO_SUPERUSER_PASSWORD=uched4123 python care_api/manage.py createsuperuser --username admin --email test@gamil.com --noinput
 unset SKIP_ZEROMQ
-python care_api/manage.py runserver 0.0.0.0:8000
+python care_api/manage.py runsslserver --certificate /cert_key/cert.pem --key /cert_key/key.pem
